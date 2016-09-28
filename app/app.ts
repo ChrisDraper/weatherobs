c5b26a613d88eece8964f167d6eb9531123b21b0
Chris Draper <Chris@Moonshine-MacBook-Air.local>
2016-09-28 20:11:45 +0100

    API get example

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ionicBootstrap, Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import { HomePage } from './pages/home/home';
 
@@ -10,12 +12,20 @@ import { HomePage } from './pages/home/home';
 })
 export class MyApp {
   rootPage: any = HomePage;
-
-  constructor(public platform: Platform) {
+  thumbnail: any;
+  constructor(public platform: Platform, public http: Http) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+
+      // API call
+      console.log('Ready');
+      this.thumbnail = null;
+      this.http.get('https://www.reddit.com/r/gifs/top/.json?limit=10').map(res => res.json()).subscribe(data => {
+          this.thumbnail = data.data.children[0].data.thumbnail;
+          console.log('Thumbnail: ' + this.thumbnail);
+      });
     });
   }
 }
