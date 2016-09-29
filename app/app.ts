71e87bed76f90fd510369b72c326e36378145a5e
Chris Draper <chris@moonshinenet.com>
2016-09-29 20:23:02 +0100

    Using OpenWeatherAPI

--- app/app.ts
+++ app/app.ts
@@ -12,19 +12,29 @@ import { HomePage } from './pages/home/home';
 })
 export class MyApp {
   rootPage: any = HomePage;
-  thumbnail: any;
+  data: any;
+  cityid: string;
+  appid: string;
   constructor(public platform: Platform, public http: Http) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-
       // API call
       console.log('Ready');
-      this.thumbnail = null;
-      this.http.get('https://www.reddit.com/r/gifs/top/.json?limit=10').map(res => res.json()).subscribe(data => {
-          this.thumbnail = data.data.children[0].data.thumbnail;
-          console.log('Thumbnail: ' + this.thumbnail);
+      this.data = null;
+      this.cityid = '2652318';
+      this.appid = '93d084a13ebef466e99ba2ccda6458f7';
+      this.http.get('http://api.openweathermap.org/data/2.5/weather?lat=51.431443&lon=-2.189674&units=metric&APPID=' + this.appid).map(res => res.json()).subscribe(data => {
+          this.data = data;
+          // Create a new JavaScript Date object based on the timestamp
+          var date = new Date(this.data.dt*1000);
+          var hours = date.getHours();
+          var minutes = "0" + date.getMinutes();
+          var formattedTime = hours + ':' + minutes.substr(-2);
+          console.log('Time: ' + formattedTime);
+          console.log('Temp: ' + this.data.main.temp + "C");
+          console.log('Weather: ' + this.data.weather[0].icon);
       });
     });
   }
