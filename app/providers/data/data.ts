b3b976689360c8cd90ff639b284d641b2ce8d005
Chris Draper <chris@moonshinenet.com>
2016-10-09 14:21:47 +0100

    Weather icons

--- app/providers/data/data.ts
+++ app/providers/data/data.ts
@@ -19,50 +19,110 @@ export class Data {
   constructor(private http: Http) {}
 
   getLatestObservation(): any {
-      //this.data = null;
-      //this.cityid = '2652318';
-      //this.appid = '93d084a13ebef466e99ba2ccda6458f7';
-      //this.http.get('http://api.openweathermap.org/data/2.5/weather?lat=51.431443&lon=-2.189674&units=metric&APPID=' + this.appid).map(res => res.json()).subscribe(data => {
-        //  this.data = data;
-          // Create a new JavaScript Date object based on the timestamp
-          //var date = new Date(this.data.dt*1000);
-         // var hours = date.getHours();
-         // var minutes = "0" + date.getMinutes();
-         // var formattedTime = hours + ':' + minutes.substr(-2);
-         // console.log('Time: ' + formattedTime);
-         // console.log('Temp: ' + this.data.main.temp + "C");
-          //console.log('Weather: ' + this.data.weather[0].icon);
-     // }); 
-      return {temp: "14C", time: "13:35", type: "snow.png"};
+      //Return a default set of results;
+      return {temp: "Loading...", time: "Loading...", type: "images/sunny.svg"};
   }
 
  formatObservation(data): any {
       this.formattedData = {time: '', temp: '', type: ''};
+      // Time
       var date = new Date(data.dt*1000);
       var hours = date.getHours();
       var minutes = "0" + date.getMinutes();
       var formattedTime = hours + ':' + minutes.substr(-2);
       this.formattedData.time = formattedTime;
+      // Temperature
       this.formattedData.temp = data.main.temp + "C";
-      this.formattedData.type = data.weather[0].icon;
+      // Weather icon
+      //01d.png      01n.png      clear sky           
+      //02d.png      02n.png      few clouds          
+      //03d.png      03n.png      scattered clouds    
+      //04d.png      04n.png      broken clouds       
+      //09d.png      09n.png      shower rain         
+      //10d.png      10n.png      rain                
+      //11d.png      11n.png      thunderstorm        
+      //13d.png      13n.png      snow                
+      //50d.png      50n.png      mist                
+      var iconImage = "images/";
+      switch (data.weather[0].icon) {
+        case "01d":
+          this.formattedData.type = iconImage + "sunny.svg";
+          break;
+        case "02d":
+          this.formattedData.type = iconImage + "scatcloud.svg";
+          break;
+        case "03d", "03n":
+          this.formattedData.type = iconImage + "lcloud.svg";
+          break;
+        case "04d", "04n":
+          this.formattedData.type = iconImage + "dcloud.svg";
+          break;
+        case "09d", "09n":
+          this.formattedData.type = iconImage + "rshower.svg";
+          break;
+        case "10d", "10n":
+          this.formattedData.type = iconImage + "rain.svg";
+          break;
+        case "11d", "11n":
+          this.formattedData.type = iconImage + "thunderstorm.svg";
+          break;
+        case "13d", "13n":
+          this.formattedData.type = iconImage + "snow.svg";
+          break;
+        case "50d", "50n":
+          this.formattedData.type = iconImage + "mist.svg";
+          break;
+        case "01n":
+          this.formattedData.type = iconImage + "night-clear.svg";
+          break;
+        case "02n":
+          this.formattedData.type = iconImage + "night-partlyclear.svg";
+          break;
+        default:
+          this.formattedData.type = iconImage + "sunny.svg";
+      }
+      console.log(data.weather[0].icon, this.formattedData.type);
+
+      // Return formatted json data
       return this.formattedData;
  }
 
   getRecentObservations(): any {
     return [
-            {type: 'snow.png',
+            {type: 'snow.svg',
             temp: '-1.2C',
             time: '2PM'},
-            {type: 'lsnow.png',
+            {type: 'lsnow.svg',
             temp: '-1.1C',
             time: '1PM'},
-            {type: 'dcloud.png',
+            {type: 'dcloud.svg',
             temp: '-1.1C',
             time: '12PM'},
-            {type: 'dcloud.png',
+            {type: 'rain.svg',
             temp: '-1.1C',
             time: '11AM'},
-            {type: 'lcloud.png',
+            {type: 'lcloud.svg',
+            temp: '-1.0C',
+            time: '10AM'},
+            {type: 'rshowers.svg',
+            temp: '-1.0C',
+            time: '10AM'},
+            {type: 'thunderstorm.svg',
+            temp: '-1.0C',
+            time: '10AM'},
+            {type: 'sunny.svg',
+            temp: '-1.0C',
+            time: '10AM'},
+            {type: 'scatcloud.svg',
+            temp: '-1.0C',
+            time: '10AM'},
+            {type: 'night-clear.svg',
+            temp: '-1.0C',
+            time: '10AM'},
+            {type: 'night-partlyclear.svg',
+            temp: '-1.0C',
+            time: '10AM'},
+            {type: 'mist.svg',
             temp: '-1.0C',
             time: '10AM'},
         ];
